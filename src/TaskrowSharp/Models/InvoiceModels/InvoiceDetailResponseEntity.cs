8d7c59bbf8955053ecbb5d4fd0e6a9d270236c5a
Eduardo Coutinho <educoutinho@gmail.com>
2025-07-28 22:28:55 -0300

    add type InternalClientAddressList in InvoiceDetailResponseEntity

--- src/TaskrowSharp/Models/InvoiceModels/InvoiceDetailResponseEntity.cs
+++ src/TaskrowSharp/Models/InvoiceModels/InvoiceDetailResponseEntity.cs
@@ -9,7 +9,7 @@ public class InvoiceDetailResponseEntity
     public Invoice InvoiceDetail { get; set; }
     public bool AllowEditInvoice { get; set; }
     public List<ClientAddress> ClientAddressList { get; set; } = [];
-    //public List<InternalClientAddressList> InternalClientAddressList { get; set; }
+    public List<InternalClientAddress>? InternalClientAddressList { get; set; } = [];
     public List<InvoiceBillType> InvoiceBillTypeList { get; set; } = [];
     public List<FinancialAccount> FinancialAccountList { get; set; } = [];
     public List<InvoiceServiceCode> InvoiceServiceCodeList { get; set; } = [];
